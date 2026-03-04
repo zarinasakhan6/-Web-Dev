1e7f9a208c01d41fc7944e4f949d331e7ad2b0fa
zarinasakhan6 <zarinasahan06@mail.ru>
2026-03-04 13:01:12 +0500

    lab6

---
+++ lab6/src/app/album/album.component.ts
@@ -0,0 +1,25 @@
+import { Component,OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HttpClient, } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
+
+@Component({
+  selector:'app-album',
+  imports:[CommonModule,RouterModule],
+  templateUrl:'./album.component.html',
+  styleUrl:'./album.component.css'
+})
+export class AlbumComponent implements OnInit{
+  albums:any[]=[];
+
+  constructor(private http:HttpClient){}
+
+  ngOnInit() {
+    this.http.get<any[]>('/assets/albums.json').subscribe({next:(data)=>{
+      this.albums=data;
+    },
+  error:(err)=>console.error('Error fetching albums:',err),});
+      
+  }
+
+}
